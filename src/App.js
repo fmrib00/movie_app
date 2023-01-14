99bb5c9ae9fd96fb4db66a899a816f6364fdcfdf
fmrib00 <yongyuezhang@gmail.com>
2023-01-14 18:06:42 +0800

    React JS Full Course 2022 | Build an App and Master React in 1 Hour https://www.youtube.com/watch?v=b9eMGE7QtTk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
+import './App.css'
+import SearchIcon from './search.png'
+import Movie from './Movie'
+
+const API_URL='http://www.omdbapi.com?apikey=db519557'
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const searchMovies = async(title) => {
+    const res = await fetch(`${API_URL}&s=${title}`)
+    const data = await res.json();
+    console.log(data)
+    if(data.Search)
+      setMovies(data.Search)
+    else if(data.Response === 'True')
+      setMovies([data])
+    else
+      setMovies([])
+  }
+  useEffect(() => { searchMovies('Batman') }, [])
+
+  const inputRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input ref={inputRef}
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && searchMovies(searchTerm)}
+        />
+        <img src={SearchIcon} alt='search' onClick={() => searchTerm && searchMovies(searchTerm)}/>
+      </div>
+      
+      {movies.length > 0 ? (
+        <div className='container'>
+          { movies.map((movie) => (
+            <Movie key={uuidv4()} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h2>Mo Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 }
